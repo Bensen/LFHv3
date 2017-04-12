68314875ef63d5715c1343ff33fe6ea1f4ed3959
Bensen <ben.schaeff@gmail.com>
2017-04-12 21:31:01 +0200

    Added the ability to delete a Character

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -12,17 +12,32 @@ class CharacterController extends Controller
         $this->middleware('auth');
     }
 
+    /**
+     * Show a list of the user's characters.
+     * 
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $user = auth()->user();
         return view('characters.index', compact('user'));
     }
 
+    /**
+     * Show the form to create a new character.
+     * 
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         return view('characters.create');
     }
 
+    /**
+     * Save a new character related to the user.
+     * 
+     * @return \Illuminate\Http\Response
+     */
     public function store()
     {
         $this->validate(request(), [
@@ -38,6 +53,52 @@ class CharacterController extends Controller
             'primary' => 'str',
             'secondary' => 'rage',
         ]));
-        return redirect('/characters');
+        return redirect()->route('characters.index');
+    }
+
+    /**
+     * Show the given character.
+     * 
+     * @param  \Character $character
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Character $character)
+    {
+        return view('characters.show', compact('character'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * 
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Delete the given character.
+     * 
+     * @param  Character $character
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Character $character)
+    {
+        $character->delete();
+        return redirect()->route('characters.index');
     }
 }
--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -14,16 +14,25 @@
                         @foreach ($user->characters as $character)
                             <div class="col-md-4">
                                 <div class="card">
-                                    <h3 class="card-header">{{ $character->name }} <small>({{ $character->level }})</small></h3>
-                                    <div class="card-block">
-                                        <img class="card-img-left" src="{{ asset('img/shadowhunter.gif') }}">
-                                        <div class="progress">
-                                            <div class="progress-bar bg-warning" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">{{ $character->experience }}/100</div>
+                                    <a href="{{ route('characters.show', ['$character' => $character->id]) }}">
+                                        <h3 class="card-header">
+                                            {{ $character->name }} <small>({{ $character->level }})</small>
+                                            <form class="float-right" method="POST" action="{{ route('characters.destroy', ['characters' => $character->id]) }}">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+                                                <button class="btn btn-danger m-0 p-2" type="submit">x</button>
+                                            </form>
+                                        </h3>
+                                        <div class="card-block">
+                                            <img class="card-img-left" src="{{ asset('img/shadowhunter.gif') }}">
+                                            <div class="progress">
+                                                <div class="progress-bar bg-warning" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">{{ $character->experience }}/100</div>
+                                            </div>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-danger" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">{{ $character->health }}/{{ $character->health }}</div>
+                                            </div>
                                         </div>
-                                        <div class="progress">
-                                            <div class="progress-bar bg-danger" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">{{ $character->health }}/{{ $character->health }}</div>
-                                        </div>
-                                    </div>
+                                    </a>
                                 </div>
                             </div>
                         @endforeach
