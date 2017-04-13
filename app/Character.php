01c9682a36969add99e7146a9a1051dc8aa8cff2
Bensen <ben.schaeff@gmail.com>
2017-04-13 19:26:07 +0200

    Markup

--- app/Character.php
+++ app/Character.php
@@ -7,15 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Character extends Model
 {
     protected $fillable = [
-        'name',
-        'character',
-        'level',
-        'experience',
-        'health',
-        'primary',
-        'secondary',
+        'name', 'fighter', 'image', 'level', 'experience', 'health', 'primary', 'secondary',
     ];
-    
+
     public function user()
     {
         return $this->belongsTo(User::class);
