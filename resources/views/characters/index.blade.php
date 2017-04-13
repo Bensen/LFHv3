60e68703959add43ed0286f0bc38a00482f5c043
Bensen <ben.schaeff@gmail.com>
2017-04-13 10:48:34 +0200

    Added Modal for deleting a Character

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -14,14 +14,31 @@
                         @foreach ($user->characters as $character)
                             <div class="col-md-4">
                                 <div class="card">
-                                    <a href="{{ route('characters.show', ['$character' => $character->id]) }}">
+                                    {{-- <a href="{{ route('characters.show', ['$character' => $character->id]) }}"> --}}
                                         <h3 class="card-header">
                                             {{ $character->name }} <small>({{ $character->level }})</small>
-                                            <form class="float-right" method="POST" action="{{ route('characters.destroy', ['characters' => $character->id]) }}">
-                                                {{ csrf_field() }}
-                                                {{ method_field('DELETE') }}
-                                                <button class="btn btn-danger m-0 p-2" type="submit">x</button>
-                                            </form>
+                                            <button class="btn btn-danger m-0 p-2 float-right" type="button" data-toggle="modal" data-target="#characterModal"><i class="fa fa-times" aria-hidden="true"></i></button>
+
+                                            <div class="modal fade" id="characterModal" tabindex="-1" role="dialog" aria-labelledby="characterModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="characterModalLabel">Charakter wirklich löschen?</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <form class="float-right" method="POST" action="{{ route('characters.destroy', ['characters' => $character->id]) }}">
+                                                                {{ csrf_field() }} {{ method_field('DELETE') }}
+                                                                <button class="btn btn-link" type="button" data-dismiss="modal">Abbrechen</button>
+                                                                <button class="btn btn-danger" type="submit">Löschen</button>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
                                         </h3>
                                         <div class="card-block">
                                             <img class="card-img-left" src="{{ asset('img/shadowhunter.gif') }}">
