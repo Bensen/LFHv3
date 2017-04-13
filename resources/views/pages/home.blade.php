a41c176597944ad09a215effd1be3d672d899519
Bensen <ben.schaeff@gmail.com>
2017-04-13 19:27:32 +0200

    Default Content

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,13 +5,14 @@
 @endsection
 
 @section('content')
-<div class="row justify-content-center">
-    <div class="col">
-        <div class="card">
-            <div class="card-block">
-                <h1 class="card-title">Startseite</h1>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-block">
+                    <h1 class="card-title">Startseite, News und so</h1>
+                    <img src="https://s-media-cache-ak0.pinimg.com/originals/36/ab/81/36ab81cd8d63cf7c4a08f39403698c77.jpg">
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
