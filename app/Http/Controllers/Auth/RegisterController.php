9cbd14cdc03eb55f9f333ab09fc91d546c86d00b
Bensen <ben.schaeff@gmail.com>
2017-04-14 17:04:52 +0200

    Revert to URL

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = route('character.index');
+    protected $redirectTo = '/character';
 
     /**
      * Create a new controller instance.
